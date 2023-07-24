ff0fc6120635db7866e1602509cc2df61a7e3243
Toan <Toan.com.vn>
2023-07-24 13:02:10 +0700

    learn React: Hook(Mounted $Unmounted)

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
+import Content from "./Content";
 import { useState } from "react";
 
 function App() {
-  const [job,setJob] = useState('');
-  const [jobs, setJobs] = useState(() => {
-  const storageJobs = JSON.parse(localStorage.getItem('jobs'));
-  return storageJobs ?? [];
-  });
-  const [checkJob, setCheckJob] = useState('');
-  
+  const [show, setShow] = useState(false);
 
-  const addList = () => {
-    const isIn = jobs.includes(job) 
-    if (!isIn){
-      setJobs(prev => {
-        const newJobs = [...prev, job];
-        const jsonJobs = JSON.stringify(newJobs);
-        localStorage.setItem('jobs', jsonJobs);
-        return newJobs
-      })
-    }else{
-      setCheckJob('has in the list');
-    }
-    setJob('');
-  }
 
   return (
     <div className="App" style={{padding:50,}}>
-      <input 
-      value={job} 
-      onChange={e => setJob(e.target.value)}
-      />
-      <button onClick={addList}>Add to List</button>
-      <p style={{color: 'red'}}>{checkJob}</p>
-      <ul>
-        {jobs.map((job, index) => {
-          return (
-            <li key={index}>{job}</li>
-          )
-          })}
-      </ul>
+      <button onClick={()=>setShow(!show)}>Toggle Content</button>
+      {show && <Content/>}
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,10 @@
+const Content = () =>{
+    return (
+        <div key='content'>
+            <h1>Mounted & Unmounted</h1>
+            <p>Đây là phương thức dùng để ẩn hiện một component khi render ra sản phẩm</p>
+        </div>
+    )
+}
+
+export default Content;
