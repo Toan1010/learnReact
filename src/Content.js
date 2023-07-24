711bac8d238623dd6f81623c2991e268ed4c795f
Toan <Toan.com.vn>
2023-07-24 16:15:47 +0700

    learn React: Hook(useEffect)

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,42 @@
+import { useEffect,useState } from "react";
+
+// 3 types of useEffect
+//useEffect(callback)
+// Goọi callback mỗi khi component rerender
+// Goọi callback sau khi component duoc render vao DOM
+//useEffect(callback, [])
+// Chi goi 1 lan sau khi component mounted
+//useEffect(callback, [deps])
+//******************* */
+
+//Call luôn được gọi sau khi Component mounted
+
 const Content = () =>{
+    const [posts, setPosts] = useState([]);
+    const [title, setTitle] = useState('');
+
+    useEffect(() =>{
+        document.title = title;
+    });
+
+    useEffect(() =>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(posts => {
+            setPosts(posts);
+            console.log(posts)
+        });
+    }, []);
+
     return (
         <div key='content'>
-            <h1>Mounted & Unmounted</h1>
-            <p>Đây là phương thức dùng để ẩn hiện một component khi render ra sản phẩm</p>
+            <input 
+                value={title}
+                onChange={(e)=>{setTitle(e.target.value)}}
+            />
+            <ul>
+                {posts.map(post => <li key={post.id}>{post.title}</li>)}
+            </ul>
         </div>
     )
 }
