56bef4a3f62fe2b3d14d6410926ceb60b9e3e7a9
Toan <Toan.com.vn>
2023-07-25 12:05:16 +0700

    learn React: Hook(useEffect with preview Avatar)

--- src/Content.js
+++ src/Content.js
@@ -11,23 +11,38 @@ import { useEffect, useState } from "react";
 //******************* */
 
 //Call luôn được gọi sau khi Component mounted
-
+//Cleanup luon duoc goi truoc khi component unmounted
+//Cleanup luon duoc goi truoc khi callback duoc goi (tru lan mounted dau tien)
 
 const Content = () =>{
-    const [countDown, setCountDow] = useState(180); 
-
-    useEffect(()=>{
-        var a = setInterval(()=>{
-            setCountDow(prev => prev - 1);
-        },1000)
-        return (()=>{
-            clearInterval(a);
-        })  
-    }, [])
+    const [avatar, setAvatar] = useState();
     
+    useEffect(() =>{
+        return(()=>{
+            avatar && URL.revokeObjectURL(avatar.preview)
+            }
+        );
+    },[avatar]);
+
+
+    
+    const handlePreview = (e) =>{
+        const file = e.target.files[0]
+        file.preview = URL.createObjectURL(file)
+        console.log(file.preview);
+        setAvatar(file);
+    };
+
     return (
         <div key='Content'>
-            <h1>{countDown}</h1>
+            <input
+                type="file"
+                onChange={handlePreview}
+            />
+            {
+                avatar && 
+                (<img src={avatar.preview} alt="none" width='80%' />)
+            }
         </div>    
     )
 }
