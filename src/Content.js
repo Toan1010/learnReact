350b18756daaa55f974bb0a91369238a33cbb7dc
Toan <Toan.com.vn>
2023-07-25 11:24:04 +0700

    learn React: Hook(useEffect wit timer)

--- src/Content.js
+++ src/Content.js
@@ -12,79 +12,22 @@ import { useEffect, useState } from "react";
 
 //Call luôn được gọi sau khi Component mounted
 
-const tabs = ['posts', 'photos', 'albums'];
 
 const Content = () =>{
-    const [type, setType] = useState('albums');
-    const [data, setData] = useState([]);
-    const [toTop, setToTop] = useState(false);
-    const [width, setWidth] = useState(window.innerWidth);
+    const [countDown, setCountDow] = useState(180); 
 
-    useEffect(() =>{
-        fetch('https://jsonplaceholder.typicode.com/'+type)
-            .then(res=>res.json())
-            .then(data => {
-                // console.log(type);
-                setData(data)
-            })
-    },[type])
-
-    useEffect(() =>{
-        const handlerScroll= () =>{
-            setToTop(window.scrollY >= 200);
-        }
-        
-        const handlerSize= () =>{
-            setWidth(window.innerWidth)
-        }
-
-
-        window.addEventListener('resize', handlerSize);
-        window.addEventListener('scroll',  handlerScroll);
-
-        //Cleanup function : la ham se goi khi doi tuong duoc goi bi unmounted.
-        //No se loai bo het cac ham ko dung toi de bo nho ko bi ro ri
-
-        return()=>{
-            window.removeEventListener('scroll', handlerScroll)
-            window.removeEventListener('resize', handlerSize)
-        }
-
-    },[]);
-
+    useEffect(()=>{
+        var a = setInterval(()=>{
+            setCountDow(prev => prev - 1);
+        },1000)
+        return (()=>{
+            clearInterval(a);
+        })  
+    }, [])
+    
     return (
         <div key='Content'>
-            {tabs.map(tab => 
-                (<button 
-                key={tab} 
-                style={type === tab ? {color:'red', background:'#333'}:{}}
-                onClick={()=>setType(tab)}
-                >
-                    {tab}
-                </button>)
-            )} 
-            
-            <h1>Present size: {width}</h1>
-            <h1>List title</h1>
-            {
-                <ul>
-                    {data.map(item => (
-                        <li key={item.id}>{item.title}</li>
-                    ))}
-                </ul>
-            }
-            {
-                toTop && (
-                    <button
-                        style={{
-                            position: 'fixed',
-                            bottom: 20,
-                            right: 20
-                        }}
-                        onClick={()=>{window.scrollTo(0,0)}}
-                    > to Top </button>
-                )
-            }
+            <h1>{countDown}</h1>
         </div>    
     )
 }
