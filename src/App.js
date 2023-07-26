d09d271586e2bda00d68576cbc185ba6ee9eb765
Toan <Toan.com.vn>
2023-07-26 12:20:26 +0700

    learn React: Hook(memo  and useCallback())

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import Content from "./Content";
-import { useState } from "react";
+import { useState, useCallback } from "react";
+
+//useCallback được dùng để tạo ra hàm dùng cho các Component con giúp cho các Component con nếu 
+//ko có sự thay đổi về CẤU TRÚC thì sẽ ko cần re render
 
 function App() {
   const [show, setShow] = useState(false);
+  const [count, setCount]= useState(1);
+  const [count2, setCount2]= useState(1);
+  
+  const handleStart = () =>{
+    setCount(prev => prev+1)
+  }
 
+  const handleStart2 = useCallback(()=>{
+    setCount2(prev => prev+1)
+  },[])
 
   return (
     <div className="App" style={{padding:50,}}>
       <button onClick={()=>setShow(!show)}>Toggle Content</button>
-      {show && <Content/>}
+      <h1>Count : {count}</h1>
+      <h1>Count2 : {count2}</h1>
+      <button 
+      onClick={handleStart}>
+        click to increase
+      </button>
+      {show && <Content 
+        handle={handleStart2}
+      />}
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,36 +1,21 @@
-import { useRef, useEffect, useState } from "react";
+import { memo } from "react";
 
-const Content = () =>{
-    const [count, setCount] = useState(60); 
-    const [disable, setDisable] = useState(false);
-    let timer = useRef({name: "timer",describe: count});
-    let prevCount = useRef();
+// memo  là một higher order component. Nó tương tự như React.PureComponent 
+// nhưng dành cho function components thay vì là class.
 
-    useEffect(() => {
-        prevCount.current = count;
-       } 
-    ,[count]);
+//memo được dùng để ngăn chặn render lại nếu như các thành phần trong Component ko có gì cần thay đổi
+//Ví dụ : khi ta dùng Content ở trong App khi App có thành phần thay đổi
+//Nếu như các thành phần trong Content ko thay đổi thì ta ko cần phải chạy lại Content 
 
-    const handleStart = () => {
-        timer.describe = setInterval(()=>{
-            setCount(prev => prev-1)
-        },1000)
-        setDisable(true);
-    }
-    const handleStop = () => {
-        clearInterval(timer.describe);
-        setDisable(false);
-    }
 
-    console.log(`prevCount:${prevCount.current} \ncount: ${count}`);
-
+const Content = ({handle}) =>{
+    console.log('render Content');
     return (
         <div key='Content'>
-            <h1>{count}</h1>
-            <button onClick={handleStart} disabled={disable}>Start</button>
-            <button onClick={handleStop}>Stop</button>
+            <h1>This is Content</h1>
+            <button onClick={handle}>Start</button>
         </div>    
     )
 }
 
-export default Content;
+export default memo(Content);
